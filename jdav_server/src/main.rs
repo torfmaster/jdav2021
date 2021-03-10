12fca2b22743ef2e7adbff3461dd8f69ae9852da
torfmaster <briefe@kebes.de>
2021-03-10 21:03:35 +0100

    Use tls

--- jdav_server/src/main.rs
+++ jdav_server/src/main.rs
@@ -3,11 +3,30 @@ mod handlers;
 mod models;
 mod routes;
 
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "jdav_server")]
+struct Options {
+    #[structopt(short, long)]
+    use_tls: bool,
+}
+
 #[tokio::main]
 async fn main() {
     let database = db::init_db().await;
 
-    warp::serve(routes::routes(database.clone()))
-        .run(([0, 0, 0, 0], 8080))
-        .await;
+    let opt = Options::from_args();
+    if opt.use_tls {
+        warp::serve(routes::routes(database.clone()))
+            .tls()
+            .cert_path("/etc/letsencrypt/live/kebes.dnshome.de/fullchain.pem")
+            .key_path("/etc/letsencrypt/live/kebes.dnshome.de/privkey.pem")
+            .run(([0, 0, 0, 0], 8080))
+            .await;
+    } else {
+        warp::serve(routes::routes(database.clone()))
+            .run(([0, 0, 0, 0], 8080))
+            .await;
+    }
 }
