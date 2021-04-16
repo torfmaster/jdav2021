a9f872eae1c637d77304d67583ccaab356d99482
torfmaster <briefe@kebes.de>
2021-04-16 12:12:16 +0200

    Do graceful shutdown

--- jdav_server/src/main.rs
+++ jdav_server/src/main.rs
@@ -5,6 +5,9 @@ mod models;
 mod routes;
 
 use structopt::StructOpt;
+use tokio::sync::oneshot::Sender;
+
+use tokio::sync::oneshot;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "jdav_server")]
@@ -13,21 +16,48 @@ struct Options {
     use_tls: bool,
 }
 
+#[derive(Debug)]
+struct ShutDownCommand;
+
 #[tokio::main]
 async fn main() {
     let database = db::db::init_db().await;
 
     let opt = Options::from_args();
+
+    let (shutdown_trigger, shutdown_command) = oneshot::channel::<ShutDownCommand>();
+
+    handle_shutdown(shutdown_trigger);
+
+    let signal_handler = async {
+        shutdown_command.await.ok();
+        println!("Gracefully shutting down");
+    };
+
     if opt.use_tls {
         warp::serve(routes::routes(database.clone()))
             .tls()
             .cert_path("/etc/letsencrypt/live/kebes.dnshome.de/fullchain.pem")
             .key_path("/etc/letsencrypt/live/kebes.dnshome.de/privkey.pem")
-            .run(([0, 0, 0, 0], 8443))
+            .bind_with_graceful_shutdown(([0, 0, 0, 0], 8443), signal_handler)
+            .1
             .await;
     } else {
         warp::serve(routes::routes(database.clone()))
-            .run(([0, 0, 0, 0], 8080))
+            .bind_with_graceful_shutdown(([0, 0, 0, 0], 8080), signal_handler)
+            .1
             .await;
-    }
+    };
+}
+
+fn handle_shutdown(tx: Sender<ShutDownCommand>) {
+    use tokio::signal::unix::{signal, SignalKind};
+
+    let term_receiver = async {
+        let mut stream = signal(SignalKind::terminate()).unwrap();
+        stream.recv().await;
+        tx.send(ShutDownCommand).unwrap();
+    };
+
+    tokio::spawn(term_receiver);
 }
